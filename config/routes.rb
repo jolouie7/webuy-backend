58e53e55a3ef086e5b22b9b2201738042d08b62e
JosephLouie <joseph161216@gmail.com>
2021-10-23 22:20:26 -0700

    Remove comments from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   root "products#index"
   resources :category_products
   resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
   resources :orders
   resources :cart_items
   resources :users
   resources :charges, only: [:create]
-  # do we need a signup route?
   post '/users', to: 'user#create'
   post '/signin', to: 'auth#create'
   get '/profile', to: 'users#profile'
